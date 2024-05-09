b6e51fb428d15ab5748e4262bef9af03ee5da2a5
MicrotersAsad <sheblumicroters@gmail.com>
2024-05-09 11:37:25 +0600

    added-updated

--- pages/index.js
+++ pages/index.js
@@ -2,9 +2,8 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import Script from 'next/script'
-import TagGenerator from './tools/tagGenerator'
-import TagExtractor from './tools/tagExtractor'
 
+import TagGenerator from './tools/tagGenerator'
 
 
 
@@ -22,9 +21,9 @@ export default function Home() {
        {/* bootstrap js cdn*/}
       <Script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></Script>
       <main className={styles.main}>
-
-      <TagExtractor/>
-      <TagGenerator/>
+    <TagGenerator/>
+    
+      
     
      
         </main>
