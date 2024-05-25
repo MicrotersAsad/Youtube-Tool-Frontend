97d96e50f2f9fd75e452cee21e59a961ebfafd9e
MicrotersAsad <sheblumicroters@gmail.com>
2024-05-25 12:22:22 +0600

    updated

--- pages/api/webhooks.js
+++ pages/api/webhooks.js
@@ -1,5 +1,5 @@
 import Stripe from 'stripe';
-import { buffer } from 'micro';
+import getRawBody from 'raw-body';
 
 const stripe = new Stripe(process.env.NEXT_PUBLIC_STRIPE_SECRET_KEY);
 
@@ -13,13 +13,13 @@ const webhookSecret = process.env.NEXT_PUBLIC_STRIPE_WEBHOOK_SECRET;
 
 export default async (req, res) => {
   if (req.method === 'POST') {
-    const buf = await buffer(req);
-    const sig = req.headers['stripe-signature'];
-
     let event;
 
     try {
-      event = stripe.webhooks.constructEvent(buf, sig, webhookSecret);
+      const rawBody = await getRawBody(req);
+      const sig = req.headers['stripe-signature'];
+
+      event = stripe.webhooks.constructEvent(rawBody, sig, webhookSecret);
     } catch (err) {
       console.error('Webhook Error:', err.message);
       return res.status(400).send(`Webhook Error: ${err.message}`);
