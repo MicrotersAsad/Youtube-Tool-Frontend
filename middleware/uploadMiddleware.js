f4d86f5aa4d32c60b64e8630fa5fab7a6b5d82a6
MicrotersAsad <sheblumicroters@gmail.com>
2024-05-28 13:04:51 +0600

    blog

--- middleware/uploadMiddleware.js
+++ middleware/uploadMiddleware.js
@@ -1,28 +1,17 @@
 import multer from 'multer';
 import path from 'path';
 
+// Define storage for the images
 const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, 'public/uploads');
+  destination: (req, file, cb) => {
+    cb(null, 'public/uploads'); // specify the destination directory
   },
-  filename: function (req, file, cb) {
+  filename: (req, file, cb) => {
     cb(null, `${Date.now()}-${file.originalname}`);
-  },
+  }
 });
 
-const upload = multer({
-  storage: storage,
-  fileFilter: function (req, file, cb) {
-    const fileTypes = /jpeg|jpg|png/;
-    const extname = fileTypes.test(path.extname(file.originalname).toLowerCase());
-    const mimetype = fileTypes.test(file.mimetype);
-
-    if (extname && mimetype) {
-      return cb(null, true);
-    } else {
-      cb('Error: Images Only!');
-    }
-  },
-});
+// Upload middleware
+const upload = multer({ storage });
 
 export default upload.single('image');
