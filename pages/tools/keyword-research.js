41295c3302bdc82bd46304c66fb2cf700c5e5837
MicrotersAsad <sheblumicroters@gmail.com>
2024-07-01 10:45:36 +0600

    update

--- pages/tools/keyword-research.js
+++ pages/tools/keyword-research.js
@@ -15,6 +15,7 @@ import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import StarRating from './StarRating';
+import Link from 'next/link';
 const KeywordSearch = () => {
   const [keyword, setKeyword] = useState('');
   const [country, setCountry] = useState('');
