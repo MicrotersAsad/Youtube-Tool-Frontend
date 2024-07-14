c1d5715ef8f8200212f8f650d3c6b079fce2f2d1
MicrotersAsad <sheblumicroters@gmail.com>
2024-07-14 17:14:21 +0600

    Update next.config.js for rewrites and image domains

--- next.config.js
+++ next.config.js
@@ -1,38 +1,23 @@
-// // next.config.js
-
 const { i18n } = require("./next-i18next.config");
 
 module.exports = {
   reactStrictMode: true,
   i18n,
-  
+
   async rewrites() {
     return [
       {
         source: '/:locale(en|fr|es)/:path*',
         destination: '/:path*',
       },
-    ];
-  },
-  
-  images: {
-    domains: ['yt3.ggpht.com','yt3.googleusercontent.com', 'i.ytimg.com','https://lobster-app-2vkuu.ondigitalocean.app/'],
-  },
-  
-  async rewrites() {
-    return [
       {
         source: '/uploads/:path*',
         destination: '/public/uploads/:path*',
       },
     ];
   },
+  
+  images: {
+    domains: ['yt3.ggpht.com','yt3.googleusercontent.com', 'i.ytimg.com', 'lobster-app-2vkuu.ondigitalocean.app'],
+  },
 };
-
-
-// // next.config.js
-// // const { i18n } = require('./next-i18next.config');
-
-// // module.exports = {
-// //   i18n,
-// // };
