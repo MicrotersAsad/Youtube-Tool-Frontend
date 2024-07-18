9c776b284b2dbaea6b13428530ae8375c31e85f4
MicrotersAsad <sheblumicroters@gmail.com>
2024-07-18 12:11:54 +0600

    updated

--- components/EditorWrapper.js
+++ components/EditorWrapper.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import PropTypes from 'prop-types';
 import dynamic from 'next/dynamic';
 import 'react-quill/dist/quill.snow.css';
@@ -8,6 +8,7 @@ const ReactQuill = dynamic(() => import('react-quill'), { ssr: false });
 
 const QuillWrapper = ({ initialContent, onChange }) => {
   const [content, setContent] = useState(initialContent || '');
+  const quillRef = useRef(null);
 
   useEffect(() => {
     setContent(initialContent);
@@ -18,28 +19,47 @@ const QuillWrapper = ({ initialContent, onChange }) => {
     onChange(value);
   };
 
+  const imageHandler = () => {
+    const url = prompt('Enter the image URL');
+    if (url) {
+      const quill = quillRef.current.getEditor();
+      const range = quill.getSelection();
+      quill.insertEmbed(range.index, 'image', url);
+    }
+  };
+
   const modules = {
-    toolbar: [
-      [{ 'header': '1' }, { 'header': '2' }, { 'header': [3, 4, 5, 6] }, { 'font': [] }],
-      [{ 'list': 'ordered' }, { 'list': 'bullet' }],
-      ['bold', 'italic', 'underline', 'strike', 'blockquote'],
-      [{ 'code-block': 'code' }],
-      ['link', 'image', 'video'],
-      [{ 'align': [] }],
-      ['clean']
-    ],
+    toolbar: {
+      container: [
+        ['bold', 'italic', 'underline', 'strike'],
+        ['blockquote', 'code-block'],
+        [{ 'header': 1 }, { 'header': 2 }],
+        [{ 'list': 'ordered' }, { 'list': 'bullet' }],
+        [{ 'script': 'sub' }, { 'script': 'super' }],
+        [{ 'indent': '-1' }, { 'indent': '+1' }],
+        [{ 'direction': 'rtl' }],
+        [{ 'size': ['small', false, 'large', 'huge'] }],
+        [{ 'header': [1, 2, 3, 4, 5, 6, false] }],
+        [{ 'color': [] }, { 'background': [] }],
+        [{ 'font': [] }],
+        [{ 'align': [] }],
+        ['clean'],
+        ['image']
+      ],
+     
+    },
   };
 
   const formats = [
     'header', 'font',
     'bold', 'italic', 'underline', 'strike', 'blockquote',
-    'list', 'bullet',
-    'code-block',
-    'link', 'image', 'video', 'align'
+    'list', 'bullet', 'script', 'indent', 'direction', 'size', 'color', 'background',
+    'link', 'image', 'video', 'align', 'code-block', 'clean'
   ];
 
   return (
     <ReactQuill
+      ref={quillRef}
       value={content}
       onChange={handleChange}
       modules={modules}
