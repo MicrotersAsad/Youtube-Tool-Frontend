63d26f709aa1679b76e3669327ee58178408f4f3
MicrotersAsad <sheblumicroters@gmail.com>
2024-07-25 17:56:03 +0600

    added

--- next.config.js
+++ next.config.js
@@ -4,34 +4,6 @@ module.exports = {
   reactStrictMode: true,
   i18n,
 
-  async redirects() {
-    return [
-      {
-        source: '/(.*)',
-        has: [
-          {
-            type: 'host',
-            value: 'www.ytubetools.com',
-          },
-        ],
-        destination: 'https://ytubetools.com/:1',
-        permanent: true,
-      },
-      {
-        source: '/(.*)',
-        has: [
-          {
-            type: 'header',
-            key: 'x-forwarded-proto',
-            value: 'http',
-          },
-        ],
-        destination: 'https://ytubetools.com/:1',
-        permanent: true,
-      },
-    ];
-  },
-
   async rewrites() {
     return [
       {
@@ -46,14 +18,6 @@ module.exports = {
   },
 
   images: {
-    domains: [
-      'yt3.ggpht.com', 
-      'yt3.googleusercontent.com', 
-      'i.ytimg.com', 
-      'https://lobster-app-2vkuu.ondigitalocean.app/',
-      'learnmethods.com',
-      '161.35.10.124',
-      'ytubetools.com'
-    ],
+    domains: ['yt3.ggpht.com', 'yt3.googleusercontent.com', 'i.ytimg.com', 'https://lobster-app-2vkuu.ondigitalocean.app/','learnmethods.com','161.35.10.124'],
   },
 };
