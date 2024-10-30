99eaaa30f452f8febd2c8fb5ac95604c586f3768
MicrotersAsad <sheblumicroters@gmail.com>
2024-10-30 17:56:59 +0600

    add

--- pages/Navbar.js
+++ pages/Navbar.js
@@ -90,6 +90,7 @@ function Navbar() {
     },
     { key: 'Pricing', href: '/pricing', dropdown: false },
     { key: 'Blog', href: '/blog', dropdown: false },
+    { key: 'Youtube', href: '/youtube', dropdown: false },
     { key: 'About Us', href: '/about', dropdown: false },
     { key: 'Contact Us', href: '/contact', dropdown: false },
   ];
