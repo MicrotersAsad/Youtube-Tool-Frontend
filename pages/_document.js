fe1e22b0092096b6394b338647bc588ad8689e3b
MicrotersAsad <sheblumicroters@gmail.com>
2024-12-03 19:57:15 +0600

    updated

--- pages/_document.js
+++ pages/_document.js
@@ -9,7 +9,7 @@ class MyDocument extends Document {
          
       
           <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" />
-          <link href="https://cdn.jsdelivr.net/npm/@wordpress/block-library@9.10.0/build-style/style.min.css" rel="stylesheet" />
+    
         
 
         </Head>
@@ -21,11 +21,7 @@ class MyDocument extends Document {
           {/* <script  src="https://code.jquery.com/jquery-3.6.0.min.js"></script> */}
           <script src="https://code.jquery.com/jquery-1.12.4.min.js" integrity="sha256-ZosEbRLbNQzLpnKIkEdrPv7lOy9C27hHQ+Xp8a4MxAQ=" crossorigin="anonymous"></script>
 
-          <script src="https://unpkg.com/@wordpress/data@latest/build/index.js"></script>
-          <script src="https://unpkg.com/@wordpress/editor@latest/build/index.js"></script>
-          <script  src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.29.1/moment.min.js"></script>
-          <script  src="https://unpkg.com/@wordpress/data@latest/build/index.js"></script>
-          <script  src="https://unpkg.com/@wordpress/editor@latest/build/index.js"></script>
+         
           
       
          
